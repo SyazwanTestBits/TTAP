df94f2391440fd2420306f767942481293ad7456
Nurfatin <nurfatin@mytestbits.com>
2023-11-01 12:01:51 +0800

    S1_TC21, 23, 25 (Changes)

--- Test Suites/Scenario 1/S1_TC015-TC020.ts
+++ Test Suites/Scenario 1/S1_TC015-TC020.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
@@ -6,14 +5,14 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>1</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>e8b3b3e4-a545-45c1-939a-d65600115c08</testSuiteGuid>
+   <testSuiteGuid>87634622-20fc-4f5e-b051-e88a478075f2</testSuiteGuid>
    <testCaseLink>
-      <guid>e1a7c2c0-d663-4fa5-8854-d68dc29982e6</guid>
+      <guid>d398b157-be06-44a7-9b97-e72185593d12</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Scenario 1/S1_TC015-Customer Place Regular Order</testCaseId>
@@ -40,6 +39,12 @@
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
          <value></value>
+         <variableId>c29bdc60-5ccb-4f85-a678-4d5d99a535fa</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
          <variableId>328207d8-4917-47f5-8364-c757fd028f27</variableId>
       </variableLink>
       <variableLink>
@@ -54,15 +59,9 @@
          <value></value>
          <variableId>e5c37b0c-9b15-4b02-b27d-82b088556d7a</variableId>
       </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>c29bdc60-5ccb-4f85-a678-4d5d99a535fa</variableId>
-      </variableLink>
    </testCaseLink>
    <testCaseLink>
-      <guid>0ca1e879-36e4-4c36-b1b3-b6d90bca17d6</guid>
+      <guid>4e16da4d-2287-44ff-be42-aaf13eaa2195</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Scenario 1/S1_TC016- Customer Download CO</testCaseId>
@@ -93,7 +92,7 @@
       </variableLink>
    </testCaseLink>
    <testCaseLink>
-      <guid>c6c5b9e1-ec7d-4e9a-a2cb-18847cba0e5c</guid>
+      <guid>3ac2ffad-8ed6-4c01-b9bc-3f502d15a17a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Scenario 1/S1_TC017- Customer Change Order</testCaseId>
@@ -148,7 +147,7 @@
       </variableLink>
    </testCaseLink>
    <testCaseLink>
-      <guid>f29b0bba-740d-462a-8df7-e4ee1a865f7e</guid>
+      <guid>2760cd8e-8cbf-4e35-ab55-b944d992dacc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Scenario 1/S1_TC018- Customer Check Change Request</testCaseId>
@@ -179,11 +178,17 @@
       </variableLink>
    </testCaseLink>
    <testCaseLink>
-      <guid>fc59c569-1c2d-4f7a-977a-4bb8f0a2dbdb</guid>
+      <guid>b1f0e13e-e5ad-4da2-b0dd-1f516d4dbc29</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Scenario 1/S1_TC019- Customer Check Order</testCaseId>
       <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>56f5e710-4e3e-40de-a7db-f5098347c4fb</variableId>
+      </variableLink>
       <variableLink>
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
@@ -194,246 +199,7 @@
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
          <value></value>
-         <variableId>56f5e710-4e3e-40de-a7db-f5098347c4fb</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
          <variableId>1ce311cf-2e90-4cec-8fc9-4a2e15d7557d</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>f672996a-294a-41e9-8dbc-83af3dd1ba32</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Scenario 1/S1_TC020- Admin Combine Order Batch</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>df21f62b-056d-4e55-80e1-c6e85b6df454</variableId>
       </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
-=======
-<?xml version="1.0" encoding="UTF-8"?>
-<TestSuiteEntity>
-   <description></description>
-   <name>S1_TC015-TC020</name>
-   <tag></tag>
-   <isRerun>false</isRerun>
-   <mailRecipient></mailRecipient>
-   <numberOfRerun>1</numberOfRerun>
-   <pageLoadTimeout>30</pageLoadTimeout>
-   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>e8b3b3e4-a545-45c1-939a-d65600115c08</testSuiteGuid>
-   <testCaseLink>
-      <guid>e1a7c2c0-d663-4fa5-8854-d68dc29982e6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Scenario 1/S1_TC015-Customer Place Regular Order</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>92d47312-b85c-4af6-a1c8-87656abfc7de</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>756bfca6-92d8-48ec-856c-c18b3974472c</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>331308fc-b7b9-457d-b9ca-096060543533</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>328207d8-4917-47f5-8364-c757fd028f27</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>3f45198a-c1b9-4369-a9d5-3e6b25033740</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>e5c37b0c-9b15-4b02-b27d-82b088556d7a</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>c29bdc60-5ccb-4f85-a678-4d5d99a535fa</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>0ca1e879-36e4-4c36-b1b3-b6d90bca17d6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Scenario 1/S1_TC016- Customer Download CO</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>c4d4bee3-29dd-46eb-9417-4219fe88da0b</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>56f5e710-4e3e-40de-a7db-f5098347c4fb</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>1e3f7796-a2fc-4539-815e-0e819309c241</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>1ce311cf-2e90-4cec-8fc9-4a2e15d7557d</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>c6c5b9e1-ec7d-4e9a-a2cb-18847cba0e5c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Scenario 1/S1_TC017- Customer Change Order</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>93cb3033-d601-48b2-b793-a0be6fe52f3a</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>cbc74f57-d96c-4055-9d30-c75ef38a19b8</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>3355d642-abc4-4af5-b3c4-728d11cc8214</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>658b78c2-abe3-4501-a24b-af22a3d0efaf</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>d6776f6a-00a2-4d2b-9602-5b7fb541d25c</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>7894c3ae-0e7f-418c-8fea-15d569d453e0</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>83ebaf64-0dd7-407b-bb1d-5c0f565a9e33</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>f2949844-8cd6-45e7-bccb-d64a05547670</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>f29b0bba-740d-462a-8df7-e4ee1a865f7e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Scenario 1/S1_TC018- Customer Check Change Request</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>7b35f55c-1735-4c5f-b889-e4355a41e1bc</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>02c9d911-69c2-41de-9a48-1cc92861fcd3</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>2f5dbbfe-7c02-4a40-a25d-06d465ccdd1b</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>d8414a1c-f1fa-457e-8d54-b2e0621aea61</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>fc59c569-1c2d-4f7a-977a-4bb8f0a2dbdb</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Scenario 1/S1_TC019- Customer Check Order</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>b314f283-5d6a-4731-9a7c-49b3ecc0a5f6</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>56f5e710-4e3e-40de-a7db-f5098347c4fb</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>1ce311cf-2e90-4cec-8fc9-4a2e15d7557d</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>f672996a-294a-41e9-8dbc-83af3dd1ba32</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Scenario 1/S1_TC020- Admin Combine Order Batch</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>df21f62b-056d-4e55-80e1-c6e85b6df454</variableId>
-      </variableLink>
-   </testCaseLink>
-</TestSuiteEntity>
->>>>>>> branch 'main' of https://gitlab.com/SyazwanTB/tb-ttap-brivge-v2.git
