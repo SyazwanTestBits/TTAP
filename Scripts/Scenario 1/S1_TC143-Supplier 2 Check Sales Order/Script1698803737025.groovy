3f4608d4a53769eab4394be767bff60c9d05a68e
Luqman <luqmanafeefi@gmail.com>
2023-11-08 12:02:20 +0800

    Resolving issue

--- Scripts/Scenario 1/S1_TC143-Supplier 2 Check Sales Order/Script1698803737025.groovy
+++ Scripts/Scenario 1/S1_TC143-Supplier 2 Check Sales Order/Script1698803737025.groovy
@@ -1,31 +1,20 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
 
-WebUI.callTestCase(findTestCase('0-Common/Login to Brivge'), [('url') : GlobalVariable.BRIVGE_URL, ('username') : GlobalVariable.BAF_USERNAME_FATIN
-	, ('password') : GlobalVariable.BAF_PWD, ('verificationCode') : GlobalVariable.VERIFICATION_CODE, ('company') : GlobalVariable.S1_BAF_BU3],
-FailureHandling.STOP_ON_FAILURE)
+WebUI.callTestCase(findTestCase('0-Common/Login to Brivge'), [('url') : GlobalVariable.BRIVGE_URL, ('username') : GlobalVariable.BAF_USERNAME_LUQMAN
+        , ('password') : GlobalVariable.BAF_PWD, ('verificationCode') : GlobalVariable.VERIFICATION_CODE, ('company') : GlobalVariable.COMPANY_SUPPLIER_1], 
+    FailureHandling.STOP_ON_FAILURE)
 
 WebUI.click(findTestObject('Object Repository/Scenario 13/S13_TC050 TC053/span_Order'))
 
-WebUI.click(findTestObject('Navbar_Brivge/OrderMenu_Brivge/li_PO Monitoring List'))
+WebUI.click(findTestObject('Navbar_Brivge/OrderMenu_Brivge/li_SO Monitoring List'))
 
-WebUI.setText(findTestObject('Scenario 12/SC12_TC014/input_contractNo'), buOrderNo)
+WebUI.setText(findTestObject('Scenario 12/SC12_TC014/input_contractNo'), SONumber)
 
 WebUI.check(findTestObject('Scenario 12/SC12_TC014/input_checkall'), FailureHandling.STOP_ON_FAILURE)
 
@@ -33,7 +22,53 @@ WebUI.click(findTestObject('Scenario 12/SC12_TC014/button_download'), FailureHan
 
 WebUI.click(findTestObject('Scenario 12/SC12_TC014/li_Download by Excel'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.verifyElementPresent(findTestObject('Scenario 12/SC12_TC014/div_Download Sales Order by Excel.The operation was successful'),
-0)
+WebUI.verifyElementPresent(findTestObject('Scenario 12/SC12_TC014/div_Download Sales Order by Excel.The operation was successful'), 
+    0)
 
 downloadedFile = CustomKeywords.'ManageFiles.getLatestFileFromDirectory'('excel')
+
+'Need to change expected file'
+CustomKeywords.'verifyExcelData.verifyDynamicSort'('C:\\Users\\luqma\\git\\tb-ttap-brivge-v2\\Excel Files\\Scenario 1\\Expected Data\\TC143\\Expected Sales Order.xlsx"', 
+    downloadedFile, 2, [23, 24, 25], [1, 2, 15, 16, 21, 17])
+
+WebUI.verifyElementText(findTestObject('Scenario 1/S1_TC143/Page_SO Monitoring List - Brivge/div_Processing'), 'Processing')
+
+WebUI.click(findTestObject('Scenario 1/S1_TC143/Page_SO Monitoring List - Brivge/button_Detail'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.waitForElementPresent(findTestObject('Scenario 1/S1_TC143/Page_SO Monitoring Detail - Brivge/h3_SO Monitoring Detail'), 
+    0)
+
+WebUI.verifyElementAttributeValue(findTestObject('Scenario 1/S1_TC143/Page_SO Monitoring Detail - Brivge/input_Please input related basic information_status'), 
+    'value', 'Completed', 0)
+
+WebUI.click(findTestObject('Scenario 1/S1_TC143/Page_SO Monitoring Detail - Brivge/button_Parts Monitoring detail_step_2'))
+
+WebUI.scrollToElement(findTestObject('Scenario 1/S1_TC143/Page_SO Monitoring Detail - Brivge/p_Copyright  2021 BriVge. All rights reserved'), 
+    0)
+
+WebUI.click(findTestObject('Page_PO_MonitoringDetail/p_Unit Parts No'))
+
+int numberrowtd = findTestData('Scenario 1/S1_TC143-Parts Detail').getRowNumbers()
+
+for (int rowl = 1; rowl <= numberrowtd; rowl++) {
+    int coll = 1
+
+    for (String colname : columnname) {
+        String valuecol = findTestData('Scenario 1/S1_TC143-Parts Detail').getValue(colname, rowl)
+
+        def actualValue = WebUI.getText(findTestObject('Scenario 1/S1_TC143/Page_SO Monitoring Detail - Brivge/div_s1001', 
+                [('lrow') : rowl, ('lcol') : coll]))
+
+        KeywordUtil.logInfo("Expected value: $valuecol ; Actual value: $actualValue")
+
+        WebUI.verifyElementText(findTestObject('Scenario 1/S1_TC143/Page_SO Monitoring Detail - Brivge/div_s1001', [('lrow') : rowl
+                    , ('lcol') : coll]), valuecol)
+
+        coll = (coll + 1)
+    }
+}
+
+WebUI.takeFullPageScreenshot()
+
+WebUI.closeBrowser()
+
