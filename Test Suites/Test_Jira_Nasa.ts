b453e209be6d73bd58468faee9fa1ac703438805
NasaTestbits <nasarudin@mytestbits.com>
2023-11-11 21:44:24 +0800

    Remove .project from tracking

--- Test Suites/Test_Jira_Nasa.ts
+++ Test Suites/Test_Jira_Nasa.ts
@@ -12,16 +12,46 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>091fa391-0a16-4f81-9a2b-70c9a077fd22</testSuiteGuid>
    <testCaseLink>
-      <guid>01b1a680-35c7-4a5f-8975-e60f826954f3</guid>
+      <guid>ea04e925-535b-41ca-ad75-a5d69fb6df5d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scenario 17/S17_TC018- Admin Combine Run Batch</testCaseId>
+      <testCaseId>Test Cases/Scenario 1/S1_TC001</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
          <value></value>
-         <variableId>da17dda5-9744-4a2d-a685-7697208c8897</variableId>
+         <variableId>4a1da43f-6925-4292-8ae6-968630c986d3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>87cfa6fc-3353-4815-b085-89414e1f6646</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6e6805f6-106f-44e8-8c89-daca6400169b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8aa19fc5-f25d-4cef-a1fc-ebf998913f12</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c035470a-17a2-445d-b0e2-d9a01fb4dcd5</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>2cc85c7f-0e46-4af2-840e-f9751690d8b4</variableId>
       </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
